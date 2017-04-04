3589212084aaaa336a9a8506af3aa04989669e0c
thagler1 <tdhagler1@gmail.com>
2017-04-04 02:06:11 +0000

    commit

--- schedule/shift_schedule/functions.py
+++ schedule/shift_schedule/functions.py
@@ -21,11 +21,11 @@ def user_oqs(user):
 
 def calcdaterange(today): #used for calendar output
 
-    month = today.strftime("%B")
+    month = today.strftime("%B") #returns just the month value
 
     first_day_of_month = datetime.datetime(today.year, today.month,1)
     fdom_weekday = first_day_of_month.weekday()
-    start_date = first_day_of_month + datetime.timedelta(days = 0-fdom_weekday)
+    start_date = first_day_of_month + datetime.timedelta(days = 0-fdom_weekday) # starts the calendar on a monday
     end_date = start_date + datetime.timedelta(days = 42)
 
     return start_date, end_date, month
@@ -44,22 +44,20 @@ def user_console_schedules(user, users_oqs):
     for console in users_oqs:
         consoles.append(console.console)
 
-    todaysdate = datetime.datetime.now()
+    todaysdate = datetime.date.today()
     start_date, end_date, month = calcdaterange(todaysdate)
-    span = (end_date - start_date)
-    timeframe = span.days
+    timeframe = (end_date - start_date).days
 
-    cal_dates = [start_date + datetime.timedelta(days=x) for x in range(timeframe)]
-    daterange = Master_schedule.objects.filter(date__range=(start_date, end_date))
+    cal_dates = [start_date + datetime.timedelta(days=x) for x in range(timeframe)] # generates list of datetime objects
+    daterange = Master_schedule.objects.filter(date__range=(start_date, end_date)) # this needs to porint to recurring events and get a return
 
     shifts = Shift.objects.all()
-    allshifts_console_schedule = [[] for y in range(7)]
-    desk_shift_name = []
-    rownum = 0
+    allshifts_console_schedule = [[] for y in range(7)] #creates 7 rows for calendar display. 
+    desk_shift_name = [] # i dont think this is used. Tagged for removal
+
     for i, date in enumerate(cal_dates):
-        display_date = date.day
-        if i%7 == 0:
-            rownum += 1
+        display_date = date.day # used to create calendar date in template
+        rownum = i//7 # no remainder division
         try:
             # look at using date__range
             if Console_schedule.objects.filter(date=date, controller=userprofile).exists():
--- schedule/shift_schedule/views.py
+++ schedule/shift_schedule/views.py
@@ -26,64 +26,6 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-def console_schedule_menu(request):
-    # this creates a simple layout showing all of the desk schedules. this is outdated. want to use for alternate
-    # views though
-    consoles = Console.objects.all()
-    template = loader.get_template('shift_schedule/console_schedule_menu.html')
-    todaysdate = datetime.datetime.now()
-    start_date = datetime.date(todaysdate.year, todaysdate.month, 1)
-    end_date = datetime.date(todaysdate.year, (todaysdate.month)+1, 1)
-    span = (end_date - start_date)
-    timeframe = span.days
-
-    cal_dates = [start_date+datetime.timedelta(days =x) for x in range(timeframe)]
-    daterange = Master_schedule.objects.filter(date__range= (start_date, end_date))
-
-    shifts = Shift.objects.all()
-    allshifts_console_schedule = []
-    desk_shift_name = []
-
-    for desk in consoles:
-        #make dictionary of schedules. html will parse the dictionary
-
-        for shift in shifts:
-
-            primary_desk_controllers = Console_oq.objects.filter(primary_console=True, console=desk)
-            active_controller = ""
-            for controller in primary_desk_controllers:
-                x = controller.controller
-
-                if x.shift == shift:
-                    desk_shift_name.append((desk, shift, x))
-                    active_controller = x
-            for date in cal_dates:
-                try:
-                    if Console_schedule.objects.filter(date = date, shift = shift,controller= active_controller).exists():
-                        console_day_schedule = Console_schedule.objects.filter(date = date,
-                                                                               shift = shift,
-                                                                               controller= active_controller)
-                        for day in console_day_schedule:
-                            allshifts_console_schedule.append((day.which_shift, shift, desk, active_controller))
-                    else:
-                        allshifts_console_schedule.append(("", shift, desk, active_controller))
-                except:
-                    pass
-
-
-
-
-    context = {
-        'consoles':consoles,
-        'daterange':daterange,
-        'cal_dates':cal_dates,
-        'shifts':shifts,
-        'allshifts_console_schedule': allshifts_console_schedule,
-        'desk_shift_name':desk_shift_name
-
-    }
-    return HttpResponse(template.render(context,request))
-
 def lexusadduser(request):
 
     #template = loader.get_template("shift_schedule/adduser.html")
