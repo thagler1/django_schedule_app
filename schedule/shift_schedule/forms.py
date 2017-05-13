29cbe458b65888e961478db591e0f30c6a685a33
thagler1 <tdhagler1@gmail.com>
2017-05-13 10:46:25 -0500

    removed datepicker

--- schedule/shift_schedule/forms.py
+++ schedule/shift_schedule/forms.py
@@ -21,7 +21,7 @@ class PTOForm(ModelForm):
 class UserprofileForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['hire_date','pto', 'manager', 'shift', 'phone', 'profile_image', 'is_supervisor', 'is_manager', 'user']
+        fields = ['hire_date','pto', 'manager', 'shift', 'phone', 'profile_image', 'user']
         widgets = {
             'hire_date':forms.DateInput(attrs={'class':'datepicker'}),
         }
