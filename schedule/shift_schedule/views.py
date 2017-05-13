09cec331cf8d13ee267c3a06575085cd70a99452
thagler1 <tdhagler1@gmail.com>
2017-05-13 10:13:54 -0500

    removed datepicker

--- schedule/shift_schedule/views.py
+++ schedule/shift_schedule/views.py
@@ -33,7 +33,7 @@ def create_user(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         uform = UserprofileForm(request.POST)
-        request.POST.get('username')
+        print(request.POST)
         if form.is_valid() and uform.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
 
