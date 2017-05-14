c7041a74c3e0ada407d0cfdc75891d553e8f07e5
thagler1 <tdhagler1@gmail.com>
2017-05-14 16:45:59 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,9 +1,14 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from celery import Celery
+from celery.schedules import crontab
 
+app = Celery()
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
-
-@shared_task
-def add(x, y):
-    return x + y
+@app.task
+def test(arg):
+    print(arg)
