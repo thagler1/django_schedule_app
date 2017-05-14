8295f27e915489fb0fb459b8c92de5ced6e2af9f
thagler1 <tdhagler1@gmail.com>
2017-05-14 16:35:10 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,11 +1,9 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from celery import Celery
-
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 
-@app.task
+
+@shared_task
 def add(x, y):
     return x + y
