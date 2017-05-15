3ca78b7f2e59e6af2afb65edfec1fc54a1ceabe4
thagler1 <tdhagler1@gmail.com>
2017-05-15 16:19:30 -0500

    added trees to scheduling classes

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -3,17 +3,12 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from celery import Celery
 from datetime import timedelta
+from .functions import increase_my_pto
+from celery.task.schedules import crontab
 
 celery = Celery(__name__)
 celery.config_from_object(__name__)
 
-@celery.task
-def say_hello():
-    print('Hello, World!')
-
-CELERYBEAT_SCHEDULE = {
-    'every-second': {
-        'task': 'example.say_hello',
-        'schedule': timedelta(seconds=5),
-    },
-}
+@periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
+def scraper_example():
+    result = increase_my_pto()
