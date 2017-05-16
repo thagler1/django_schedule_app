1a2a7e268ab93b9f9e6a85c7fd808adc21389bb9
thagler1 <tdhagler1@gmail.com>
2017-05-16 01:42:56 -0500

    added trees to scheduling classes

--- schedule/schedule/celery.py
+++ schedule/schedule/celery.py
@@ -5,14 +5,13 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule.settings')
-app = Celery('shift_schedule')
+app = Celery('schedule')
 
 # Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# pickle the object when using Windows
 
-
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,20 +1,14 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
 from celery import Celery
-from datetime import timedelta
 from .functions import increase_my_pto
-from celery.schedules import crontab
 
+from celery import shared_task
 celery = Celery(__name__)
 celery.config_from_object(__name__)
 
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender,**kawrgs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test(), name='add every 10')
-
-@app.task
-def test():
+@shared_task
+def add_pto():
+    increase_my_pto()
     increase_my_pto()
