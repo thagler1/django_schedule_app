ceace52e3d65376abdc6e74ddfffd819bb49d3fb
thagler1 <tdhagler1@gmail.com>
2017-05-16 19:56:29 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,14 +1,22 @@
-# Create your tasks here
-from __future__ import absolute_import, unicode_literals
 from celery import Celery
-from .functions import increase_my_pto
-
-from celery import shared_task
-celery = Celery(__name__)
-celery.config_from_object(__name__)
+from celery.schedules import crontab
 
 app = Celery()
-@shared_task
-def add_pto():
-    increase_my_pto()
-    increase_my_pto()
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+@app.task
+def test(arg):
+    print(arg)
