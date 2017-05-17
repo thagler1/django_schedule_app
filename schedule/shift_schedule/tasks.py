493bfa0816ccf2d9d38594bc2cced666a33b7267
thagler1 <tdhagler1@gmail.com>
2017-05-17 02:25:18 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -7,6 +7,7 @@ from celery.schedules import crontab
 @shared_task
 def add():
     print("It made it here")
+    '''
     from .models import UserProfile
     from django.contrib.auth.models import User  # used fro user profiles
     user = User.objects.get(first_name = 'Todd')
@@ -14,6 +15,8 @@ def add():
     up.pto += 10
     up.save()
     return up.pto
+    '''
+    return 3
 
 @shared_task
 def celery_is_awful():
