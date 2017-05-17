305ade972383377f2d724e1b1c1e57afe4f8b8f6
thagler1 <tdhagler1@gmail.com>
2017-05-17 02:11:17 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from celery import Celery
+from celery.schedules import crontab
 
-
-
+@app.on_after_configure.connect
 
 
 @shared_task
@@ -13,5 +14,5 @@ def add():
     user = User.objects.get(first_name = 'Todd')
     up = UserProfile.objects.get(id = user.id)
     up.pto += 10
-    up.save
+    up.save()
     return up.pto
