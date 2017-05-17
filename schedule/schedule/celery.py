c585c772e59fdc8bcd8f624770bc2316cf39b6b7
thagler1 <tdhagler1@gmail.com>
2017-05-16 19:11:24 -0500

    chenged celery settings

--- schedule/schedule/celery.py
+++ schedule/schedule/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule.settings')
@@ -12,6 +13,10 @@ app = Celery('schedule')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+
+
+app.conf.beat_schedule = {
+    'add_pto_test':{
+        'task':'test',
+        'schedule':crontab(),}
+}
