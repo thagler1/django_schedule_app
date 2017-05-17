03a9379c4883dee8e0875c5952a1d145887f85da
thagler1 <tdhagler1@gmail.com>
2017-05-16 21:50:19 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+from celery import Celery
 
-
-@shared_task
+app = Celery()
+@app.task(ignore_result=True)
 def add():
     print("Hi")
     return True
