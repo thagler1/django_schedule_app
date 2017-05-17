906c13519ef426bcc76ac3c94e94129e1e89d911
thagler1 <tdhagler1@gmail.com>
2017-05-17 01:05:43 -0500

    chenged celery settings

--- schedule/shift_schedule/tasks.py
+++ schedule/shift_schedule/tasks.py
@@ -1,12 +1,6 @@
-from __future__ import absolute_import, unicode_literals
-CELERY_IMPORTS = ('shift_schedule.models', )
-import os
-from celery import Celery
 from schedule.shift_schedule.models import UserProfile
 from django.contrib.auth.models import User #used fro user profiles
 
-
-app = Celery()
 @app.task(ignore_result=True)
 def addpto():
     user = User.objects.get(first_name = 'Todd')
