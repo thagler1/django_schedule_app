c698de066642a70f20e11f3585038fb3074420dc
thagler1 <tdhagler1@gmail.com>
2017-05-17 23:08:25 -0500

    chenged celery settings

--- schedule/shift_schedule/urls.py
+++ schedule/shift_schedule/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^unapproved_pto', views.unnaproved_pto, name ='unapproved_pto'),
     url(r'^shift_supervisor_console', views.supervisors_console, name ='supervisor_console'),
     url(r'^debug',views.debugpage,name='debug'),
-    url(r'^test', views.controller_pto_form(),name='test')
+    url(r'^test', views.controller_pto_form,name='test')
 
 
 
