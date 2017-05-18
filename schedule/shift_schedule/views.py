53c56aa37141ed1ae8159941d29236a786292744
thagler1 <tdhagler1@gmail.com>
2017-05-17 23:39:08 -0500

    chenged celery settings

--- schedule/shift_schedule/views.py
+++ schedule/shift_schedule/views.py
@@ -259,7 +259,7 @@ def debugpage(request):
 def add_console(request):
 
     if request.method =='POST':
-        form = ConsoleForm(request)
+        form = ConsoleForm(request.POST)
         if form.is_valid():
             form.save()
 
