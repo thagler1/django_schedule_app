3442d9f1df71c10e3ef5479afedac2ca4115a5b8
thagler1 <tdhagler1@gmail.com>
2017-06-04 04:03:55 -0500

    removed namespace arg in celery conf

--- schedule/schedule/celery.py
+++ schedule/schedule/celery.py
@@ -12,8 +12,8 @@ app = Celery('schedule')
 # pickle the object when using Windows
 
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks()
 
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
