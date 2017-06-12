80f65e40b59785874de1fbfbed1c82b92ddd8858
thagler1 <tdhagler1@gmail.com>
2017-06-12 08:02:44 -0500

    changed login page

--- schedule/shift_schedule/urls.py
+++ schedule/shift_schedule/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.user_login, name='index'),
     url(r'adduser', views.create_user, name ="adduser"),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^user/(?P<calmonth>[0-9]{2})', views.user_page, name ='userpage'),
