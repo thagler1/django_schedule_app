c7601f485009078f87fcf2ae0591fef645294fb5
thagler1 <tdhagler1@gmail.com>
2017-06-27 02:59:08 -0500

    removed unnecessary imports

--- schedule/shift_schedule/urls.py
+++ schedule/shift_schedule/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import url
 from . import views
 from .sms import incoming
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
